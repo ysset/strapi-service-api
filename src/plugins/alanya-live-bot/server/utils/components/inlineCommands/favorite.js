6dcc3c5f401b49839b73f6c4c5b41306658beff2
ysset <kamdenech@gmail.com>
2022-07-14 12:04:02 +1200

    Added edit message instead of send message

--- src/plugins/alanya-live-bot/server/utils/components/inlineCommands/favorite.js
+++ src/plugins/alanya-live-bot/server/utils/components/inlineCommands/favorite.js
@@ -3,8 +3,11 @@ const { localisation, userLang } = require('../../../../../botUtils/botsLanguage
 module.exports = async (query) => {
     localisation.current = query.from.language_code;
     const chatId = query.message?.chat.id || query.chat.id;
+    const messageId = query.message?.message_id || query.message_id;
 
-    return await strapi.bots.alanyaBot.sendMessage(chatId, 'Выберите подгруппу', {
+    return await strapi.bots.alanyaBot.editMessageText('Выберите подгруппу', {
+        chat_id: chatId,
+        message_id: messageId,
         reply_markup: {
             inline_keyboard: [
                 [
