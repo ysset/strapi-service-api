e0cd624688335386d18ea00eb0e737182ac3b78e
ysset <kamdenech@gmail.com>
2022-07-15 16:41:38 +1200

    Added postgres db

--- config/database.js
+++ config/database.js
@@ -1,11 +1,13 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+        client: 'postgres',
+        connection: {
+            host: env('DATABASE_HOST', '127.0.0.1'),
+            port: env.int('DATABASE_PORT', 5432),
+            database: env('DATABASE_NAME', 'xattaBot'),
+            user: env('DATABASE_USERNAME', 'develop'),
+            password: env('DATABASE_PASSWORD', 'developpass'),
+        },
+        debug: false,
     },
-    useNullAsDefault: true,
-  },
 });
