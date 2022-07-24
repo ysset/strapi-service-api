2b12f6ec5ecab3742af1a17d3ba2dd6a83102003
ysset <kamdenech@gmail.com>
2022-07-24 15:03:43 +1200

    Fixed admin url

--- src/plugins/xatta-realtor-admin-bot/botUtils/language/en.js
+++ src/plugins/xatta-realtor-admin-bot/botUtils/language/en.js
@@ -3,5 +3,5 @@ module.exports = {
     GET_USERNAME: 'Please submit a name that will be visible to users',
     GET_USER_EMAIL: 'Please enter your email',
     COMPLETE_ADMIN_REGISTRATION: (registrationToken) =>
-        `You now have access to the xatta admin panel.\nhttp://xatta.ru//admin/auth/register?registrationToken=${registrationToken} \n Please proceed with registration by clicking on this link`,
+        `You now have access to the xatta admin panel.\nhttp://xatta.ru/admin/auth/register?registrationToken=${registrationToken} \n Please proceed with registration by clicking on this link`,
 };
--- src/plugins/xatta-realtor-admin-bot/botUtils/language/ru.js
+++ src/plugins/xatta-realtor-admin-bot/botUtils/language/ru.js
@@ -3,5 +3,5 @@ module.exports = {
     GET_USERNAME: 'Пожалуйста отправьте имя которое будут видеть ваши клиенты',
     GET_USER_EMAIL: 'Пожалуйста отправьте свой email',
     COMPLETE_ADMIN_REGISTRATION: (registrationToken) =>
-        `Теперь у вас есть доступ к xatta админке.\nhttp://xatta.ru//admin/auth/register?registrationToken=${registrationToken} \n Пожалуйста, продолжите регистрацию перейдя по этой ссылке.`,
+        `Теперь у вас есть доступ к xatta админке.\nhttp://xatta.ru/admin/auth/register?registrationToken=${registrationToken} \n Пожалуйста, продолжите регистрацию перейдя по этой ссылке.`,
 };
