0d93f0221efd4c25a87720678eb142331dd18ead
ysset <kamdenech@gmail.com>
2022-08-30 00:46:02 +1200

    Hot fixes

--- scripts/dump.sh
+++ scripts/dump.sh
@@ -1,9 +1,10 @@
 pm2 stop all
 
+mkdir ../dumps
 mkdir ../dumps/dump_$(date '+%d-%m-%Y_%H')
 docker-compose exec DB pg_dump -c -U develop xattaBot --no-owner > ../dumps/dump_$(date '+%d-%m-%Y_%H')/db-dump.sql
 
-zip uploads.zip ../public/*
+zip uploads.zip ../public/uploads/*
 mv uploads.zip ../dumps/dump_$(date '+%d-%m-%Y_%H')
 
 pm2 start all
