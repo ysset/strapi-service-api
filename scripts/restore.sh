f9c07059a8a0df16a76dcc437cae2c6c045d9323
ysset <kamdenech@gmail.com>
2022-08-30 01:05:17 +1200

    Hot fixes

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,9 +1,11 @@
-pm2 stop all
-
-echo "enter dump file name from 'dumps' file"
+echo "enter dump file name from 'dumps' file on production server "
 read filePath
-pm2 stop all
-cat ../dumps/${filePath}/db-dump.sql | docker-compose exec -T DB psql -U develop xattaBot
-unzip ../dumps/${filePath}/uploads.zip
-mv ./public ../
+rm -rf ../dumps
+mkdir ../dumps
+scp root@217.114.43.187:~/TelegramBot/dumps/${filePath}/uploads.zip ../dumps
+scp root@217.114.43.187:~/TelegramBot/dumps/${filePath}/db-dump.sql ../dumps
 
+cat ../dumps/db-dump.sql | docker-compose exec -T DB psql -U develop xattaBot
+rm -rf ../public
+unzip ../dumps/uploads.zip
+mv ./public ../
