fbd545fcf4bb0a6bf39210643921307c7365bb81
ysset <kamdenech@gmail.com>
2022-09-02 23:15:41 +1200

    Hot fix

--- config/database.js
+++ config/database.js
@@ -3,8 +3,8 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         connection: {
             host: env('DATABASE_HOST', '127.0.0.1'),
-            port: env.int('DATABASE_PORT', 5433),
-            database: env('DATABASE_NAME', 'testXattaBot'),
+            port: env.int('DATABASE_PORT', 5432),
+            database: env('DATABASE_NAME', 'xattaBot'),
             user: env('DATABASE_USERNAME', 'develop'),
             password: env('DATABASE_PASSWORD', 'developpass'),
         },
