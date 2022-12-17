a3575c496fbec1c6e6042698fee6c1b3118388d4
DGLutsky <dglutsky@greenatom.ru>
2022-12-17 19:39:06 +1200

    Обновлена англ локализация

--- src/plugins/localisation/en/realtor.js
+++ src/plugins/localisation/en/realtor.js
@@ -21,7 +21,10 @@ module.exports = {
         text: 'I want to take a free tour 🚀!',
     },
     INF_TOUR: 'You are signed up for a free tour! Our manager will contact you soon!',
-    INF_TOUR_REALTOR: ({ username }) => `@${username} wants info tour`,
+    INF_TOUR_REALTOR: ({ username, phoneNumber }) =>
+        `User wants info tour\n` +
+        `${username ? `@${username}` : ''}\n` +
+        `${phoneNumber ? `${phoneNumber}` : ''}`,
     CANCEL_INFO_TOUR_INLINE: {
         text: `'I don't want to go to info tour'`,
     },
