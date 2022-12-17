a388dac7e055e90eea3e12eb49ee04e6816f2259
DGLutsky <dglutsky@greenatom.ru>
2022-12-17 18:01:53 +1200

    Добавлена проверка на сохраненные объекты в подробном описании

--- src/plugins/realtor-bot/server/bot/components/inlineCommands/searchFullDescription.js
+++ src/plugins/realtor-bot/server/bot/components/inlineCommands/searchFullDescription.js
@@ -6,43 +6,65 @@ module.exports = async (bot) => {
         localisation,
         chatId,
         data: { table, flatId },
+        user,
     } = bot;
 
+    const favorites = [
+        ...user.favoriteVilla,
+        ...user.favoriteOwner,
+        ...user.favoriteComplex,
+        ...user.favoriteRent,
+    ];
+
+    const isFlatFavorite = favorites.find((el) => el.id === flatId);
+    console.log(isFlatFavorite, favorites, user);
+    const inline_keyboard = [
+        [
+            {
+                ...localisation?.WRITE_INLINE[table.toLowerCase()],
+                callback_data: JSON.stringify({
+                    action: actions.SEARCH_WRITE_AGENT_FOR_FULL_DESCRIPTION,
+                    table,
+                    flatId,
+                }),
+            },
+        ],
+        !isFlatFavorite
+            ? [
+                  {
+                      ...localisation?.SAVE_INLINE,
+                      callback_data: JSON.stringify({
+                          action: actions.SAVE,
+                          table,
+                          flatId,
+                      }),
+                  },
+                  {
+                      ...localisation?.NEXT_INLINE,
+                      callback_data: JSON.stringify({
+                          action: actions.SFDNF,
+                          table,
+                          flatId,
+                      }),
+                  },
+              ]
+            : [
+                  {
+                      ...localisation?.NEXT_INLINE,
+                      callback_data: JSON.stringify({
+                          action: actions.SFDNF,
+                          table,
+                          flatId,
+                      }),
+                  },
+              ],
+    ];
     const { caption, messages } = await fullDescription(bot);
     const { message_id: callbackMessage } = await bot
         .sendMessage(chatId, caption, {
             parse_mode: 'HTML',
             reply_markup: {
-                inline_keyboard: [
-                    [
-                        {
-                            ...localisation?.WRITE_INLINE[table.toLowerCase()],
-                            callback_data: JSON.stringify({
-                                action: actions.SEARCH_WRITE_AGENT_FOR_FULL_DESCRIPTION,
-                                table,
-                                flatId,
-                            }),
-                        },
-                    ],
-                    [
-                        {
-                            ...localisation?.SAVE_INLINE,
-                            callback_data: JSON.stringify({
-                                action: actions.SAVE,
-                                table,
-                                flatId,
-                            }),
-                        },
-                        {
-                            ...localisation?.NEXT_INLINE,
-                            callback_data: JSON.stringify({
-                                action: actions.SFDNF,
-                                table,
-                                flatId,
-                            }),
-                        },
-                    ],
-                ],
+                inline_keyboard,
             },
         })
         .catch(console.error);
