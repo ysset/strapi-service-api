7267d58c7d4c4ddd4b3da77ef04bc7ec5201b9d5
DGLutsky <dglutsky@greenatom.ru>
2024-01-28 05:12:13 +1200

    Transferred email transporter to bootstrap.js

--- src/plugins/bot-console/server/bootstrap.js
+++ src/plugins/bot-console/server/bootstrap.js
@@ -1,5 +1,26 @@
 'use strict';
 
+const nodemailer = require("nodemailer");
 module.exports = ({ strapi }) => {
-    // bootstrap phase
+    const transporter = nodemailer.createTransport({
+        service: 'Yandex',
+        host: process.env.YANDEX_SMTP_HOST,
+        port: process.env.YANDEX_SMTP_PORT,
+        secure: true,
+        auth: {
+            user: process.env.YANDEX_SMTP_USER,
+            pass: process.env.YANDEX_SMTP_USER_PASS,
+        },
+    });
+
+    transporter.verify(function (error, success) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Server is ready to take our messages");
+        }
+    });
+
+    strapi.mail = transporter;
+    return strapi;
 };
--- src/plugins/bot-console/server/services/AddNewAuthor.js
+++ src/plugins/bot-console/server/services/AddNewAuthor.js
@@ -1,26 +1,5 @@
 'use strict';
 const { v4 } = require('uuid');
-const nodemailer = require("nodemailer");
-require('dotenv').config()
-
-const transporter = nodemailer.createTransport({
-    service: 'Yandex',
-    host: process.env.YANDEX_SMTP_HOST,
-    port: process.env.YANDEX_SMTP_PORT,
-    secure: true,
-    auth: {
-        user: process.env.YANDEX_SMTP_USER,
-        pass: process.env.YANDEX_SMTP_USER_PASS,
-    },
-});
-
-transporter.verify(function (error, success) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log("Server is ready to take our messages");
-    }
-});
 
 module.exports = ({ strapi }) => ({
     async AddNewAuthor(ctx) {
@@ -63,11 +42,11 @@ module.exports = ({ strapi }) => ({
             })
 
             strapi.log.info(`Created author: ${firstname} ${lastname} (${email})`);
-            await transporter.sendMail({
+            await strapi.mail.sendMail({
                 from: process.env.YANDEX_SMTP_FROM,
                 to: "kamdenech@gmail.com",
-                subject: 'Hello',
-                text: "xyi"
+                subject: 'xaxa',
+                text: "testing meailer"
             }, console.log)
             return ctx.send({ message: 'Bot created successfully!' }, 200);
         } catch (err) {
