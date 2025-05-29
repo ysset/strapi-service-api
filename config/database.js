a917346858833608164646f24f407a8e501e70fc
Ysset <kamdenech@gmail.com>
2025-05-29 20:06:06 +0300

    db fix

--- config/database.js
+++ config/database.js
@@ -7,8 +7,8 @@ module.exports = ({ env }) => {
     mysql: {
       connection: {
         connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'some-mongo'),
-        port: env.int('DATABASE_PORT', 27017),
+          host: env('DATABASE_HOST', 'localhost'),
+          port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
